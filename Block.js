77519ae649141c34628159a397e6ae6a79cd759f
AaradhyVyas <77629845+AaradhyVyas@users.noreply.github.com>
2021-05-29 15:46:55 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -4,6 +4,7 @@ class Block{
           restitution :0.4,
           friction :0.0,
       }
+       this.Visibility = this.Visibility=225;
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
@@ -23,7 +24,6 @@ class Block{
     else{
       World.remove(world, this.body);
       push();
-      this.Visibility = this.Visibility-5;
       tint(255,this.Visibility);
       image(this.image,this.body.position.x,this.body.position.y,50,50);
       pop();
