bd0ab769b15187a9af531a3c78e29f3391f838c4
AaradhyVyas <77629845+AaradhyVyas@users.noreply.github.com>
2021-07-16 13:39:34 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,34 +1,30 @@
 class Block{
   constructor(x, y, width, height) {
-    this.image = loadImage("block.png");
+    
       var options = {
           restitution :0.4,
           friction :0.0,
-        this.Visibility = this.Visibility=225;
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.image = loadImage("block.png");
+      this.Visibility=255;
       World.add(world, this.body);
     }
     display(){
-    if(this.body.speed<5){      
-      var angle= this.body.angle;
-      var pos= this.body.position;
-      push();
-      this.Visibility = this.Visibility=-5;
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      rect(0,0,this.width, this.height);
-      pop();
-    }
-    else{
+      if(this.body.speed <3){ 
+        imageMode(CENTER);
+         image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+         }
+    else{ 
       World.remove(world, this.body);
       push();
+      this.Visibility=this.Visibility-5;
       tint(255,this.Visibility);
       image(this.image,this.body.position.x,this.body.position.y,30,40);
       pop();
     }
-    }
+    
+  }
 }
